aed435381ddb820bc24b0515dda87ade6eb09bc3
Darin Pytel <CinnamonRoll@post.cz>
2023-10-27 19:01:25 +0200

    AAAAAA

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hello World!\n");
     printf("Mám rád avokáda a pomeranče!\n");
-    printf("Ahoj, tady Darin.\n");
+    printf("Ahoj, tady stále je Darin.\n");
     printf("Ahoj, tady Astra.\n");
 }
