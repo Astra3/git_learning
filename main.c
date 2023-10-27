f4090d014c07429f8061e844e57deb8698af8bb9
Darin Pytel <CinnamonRoll@post.cz>
2023-10-27 19:33:27 +0200

    darinuv branch creation

--- main.c
+++ main.c
@@ -2,7 +2,5 @@
 
 int main() {
     printf("Hello World!\n");
-    printf("Mám rád avokáda a pomeranče!\n");
-    printf("Ahoj, tady stále je Darin.\n");
-    printf("Ahoj, tady Astra.\n");
+    printf("TOTO JE MŮJ BRANCH!\n");
 }
